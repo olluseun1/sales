e17c4b45a1dda76847674a38dddc69664f39ec18
olu <ollu1.ajani@gmail.com>
2023-07-08 14:58:40 +0100

    add more variables

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 module "provision_ec2_1" {
   source = "git::https://github.com/olluseun1/module.git"
+  machine_name = var.sales_1_machine_name
+  machine_key = var.sales_1_key_name
 }
 
 module "provision_ec2_2" {
   source = "git::https://github.com/olluseun1/module.git"
-  machine_name = "second machine"
+  machine_name = var.sales_2_machine_name
+  machine_key = var.sales_2_key_name
 }
 
 terraform {
@@ -20,3 +23,8 @@ provider "aws" {
   #profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region = "eu-west-1"
 }
+
+sales_1_machine_name {}
+sales_1_key_name {}
+sales_2_machine_name {}
+sales_2_key_name {}
